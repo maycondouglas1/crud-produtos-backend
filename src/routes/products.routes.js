65b55085458a1c624695750c2b5d817385cec4c1
Maycon Douglas <mayconmrqs2001@gmail.com>
2022-06-12 12:49:26 -0300

    fix

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,11 +3,6 @@ import ProductController from '../controllers/products.controller.js';
 
 const router = Router();
 
-// Default route
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
 router.get('/products', ProductController.findAll);
 router.post('/products', ProductController.create);
 router.put('/products/:id', ProductController.update);
